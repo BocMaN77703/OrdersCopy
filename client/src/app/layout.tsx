262e8a4cfb19477970f5d6535a83b0b4b9abd07f
vlados2108 <verlov.vladislav@mail.ru>
2023-09-17 16:31:27 +0300

    replaced body code with imported component Header

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,39 +1,14 @@
 'use client'
-import { useRouter, usePathname } from 'next/navigation'
-import { GlobalContextProvider, useGlobalContext } from './context/store'
-import { useContext, useState } from 'react'
-import Modal from './modal/modal'
-import axios from 'axios'
+
 import { CookiesProvider } from 'react-cookie'
-import Link from 'next/link'
+import { GlobalContextProvider } from './context/store'
+import Header from './header'
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const [isModalActive, setIsModalActive] = useState(false)
-    const [hasAccessToken, setHasAccessToken] = useState(false)
-    const [code, setCode] = useState('')
-    const router = useRouter()
-    const pathName = usePathname()
-
-    const host = process.env.HOST
-
-    const login = () => {
-        axios.post(`${host}/auth/login/${code}`, {}, { withCredentials: true })
-        setHasAccessToken(true)
-        setIsModalActive(false)
-        router.refresh()
-    }
-
-    const logout = () => {
-        axios.post(`${host}/auth/log-out/`, {}, { withCredentials: true })
-        setHasAccessToken(false)
-        setIsModalActive(false)
-        router.refresh()
-    }
-
     return (
         <CookiesProvider>
             <html lang="en">
@@ -45,89 +20,7 @@ export default function RootLayout({
                 </head>
                 <body>
                     <GlobalContextProvider>
-                        <div className="main-container">
-                            <div className="main-header">
-                                {pathName == '/' ? (
-                                    <Link
-                                        className="header-text"
-                                        href={'/Cart'}
-                                    >
-                                        Cart
-                                    </Link>
-                                ) : (
-                                    <div
-                                        className="header-text"
-                                        onClick={() => {
-                                            router.push('/')
-                                        }}
-                                    >
-                                        Catalog
-                                    </div>
-                                )}
-                                <div
-                                    className="header-text"
-                                    onClick={() => {
-                                        setIsModalActive(true)
-                                    }}
-                                >
-                                    {hasAccessToken == true
-                                        ? 'Log out'
-                                        : 'Log In'}
-                                </div>
-                                <div
-                                    className="header-text"
-                                    onClick={() => {
-                                        router.push('/orders')
-                                    }}
-                                >
-                                    Orders
-                                </div>
-                                <div
-                                    className="header-text"
-                                    onClick={() => {
-                                        router.push('/confirmedOrders')
-                                    }}
-                                >
-                                    Confirmed orders
-                                </div>
-                            </div>
-
-                            {children}
-                        </div>
-                        <Modal
-                            active={isModalActive}
-                            setActive={setIsModalActive}
-                        >
-                            {!hasAccessToken ? (
-                                <div className="login-container">
-                                    <input
-                                        className="login-input"
-                                        value={code}
-                                        onChange={(e) =>
-                                            setCode(e.target.value)
-                                        }
-                                    ></input>
-                                    <button
-                                        className="login-btn"
-                                        onClick={login}
-                                    >
-                                        Log in
-                                    </button>
-                                </div>
-                            ) : (
-                                <div className="login-container">
-                                    <div className="log-out-question">
-                                        Are you sure u want to log out?
-                                    </div>
-                                    <button
-                                        className="login-btn"
-                                        onClick={logout}
-                                    >
-                                        Yes
-                                    </button>
-                                </div>
-                            )}
-                        </Modal>
+                        <Header children={children}></Header>
                     </GlobalContextProvider>
                 </body>
             </html>
